b457f6edb669445420ae363de4e8ec75a7f8884c
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-02-28 19:41:42 +0530

    initial commit adding boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname + "/dist/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["react", "es2015", "stage-1"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "./"
+  }
+};
