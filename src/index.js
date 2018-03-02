00c1095c31eb95ee5998a2bdf17df3dd12fb2a15
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-02 16:23:08 +0530

    Added boilerplate code for .scss file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+
 import App from "./components/App.js";
+import "../style/style.scss";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
